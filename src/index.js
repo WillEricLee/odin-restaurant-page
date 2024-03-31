726dee72e7d4d3bffe72f02ee7aad6542b9dc139
Will Lee <willericlee@gmail.com>
2024-03-31 01:03:32 -0700

    Create page-switching logic, modules for each page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import homePage from './homepage.js';
+import menuPage from './menupage.js';
+import aboutPage from './aboutpage.js';
 import './style.css';
+
+const content = document.querySelector('#content');
+
+const homeButton = document.querySelector(".home");
+const menuButton = document.querySelector('.menu');
+const aboutButton = document.querySelector('.about');
+
+homeButton.onclick = () => {
+    content.innerHTML = "";
+    homePage();
+};
+menuButton.onclick = () => {
+    content.innerHTML = "";
+    menuPage();
+};
+aboutButton.onclick = () => {
+    content.innerHTML = "";
+    aboutPage();
+};
+
 
 console.log('test');
 
